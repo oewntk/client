6e08c60a91f356ba2ce052d43e977f23aa200090
1313ou <1313ou@gmail.com>
2026-07-18 09:31:58 +0200

    wip server/client

--- src/main/java/org/oewntk/json/client/main.kt
+++ src/main/java/org/oewntk/json/client/main.kt
@@ -31,58 +31,72 @@ fun queryObject() {
         }
 }
 
-fun queryText0() {
-    val client = Client(endpoint)
-    client.queryText("/", "")?.let { println("[OEWN] $it") }
+val allOptions = listOf(null, "mode=model", "mode=oewn", "mode=data", "mode=data,method=typed")
 
-    listOf("00001740-a", "00001740-n", "00001740-r", "00001740-v")
-        .forEach { id ->
-            client.queryText("/api/synset/", id, "mode=data, method=typed")?.let { println("[JSON SYNSET] $it") }
-        }
-
-    listOf("row%1:14:00::", "row%1:17:00::", "row%1:06:00::", "row%1:14:01::", "row%1:07:00::", "row%1:04:00::", "row%1:10:00::", "row%2:38:00::")
-        .forEach { id ->
-            client.queryText("/api/sense/", id)?.let { println("[JSON SENSE] $it") }
-        }
-
-    listOf("row,n-1", "row,n-2", "row,v")
-        .forEach { id ->
-            client.queryText("/api/lex/", id)?.let { println("[JSON LEX] $it") }
-        }
-
-    listOf("row", "grow")
-        .forEach { id ->
-            client.queryText("/api/word/", id)?.let { println("[JSON LEMMA] $it") }
-        }
-}
-
-fun queryText() {
+fun queryText1() {
     val client = Client(endpoint)
     client.queryText("/", "")
 
     listOf("00001740-n")
         .forEach { id ->
-            //client.queryText("/api/synset/", id)?.let { println("[JSON SYNSET] $it") }
-            //client.queryText("/api/synset/", id, "mode=model")?.let { println("[JSON SYNSET] $it") }
-            client.queryText("/api/synset/", id, "mode=data, method=typed")?.let { println("[JSON SYNSET] $it") }
-            //client.queryText("/api/synset/", id, "mode=data")?.let { println("[JSON SYNSET] $it") }
-            //client.queryText("/api/synset/", id, "mode=oewn")?.let { println("[JSON SYNSET] $it") }
+            allOptions.forEach { options ->
+                client.queryText("/api/synset/", id, options = options)?.let { println("[JSON SYNSET $options]\n$it") }
+            }
         }
 
-    //listOf("row%1:14:00::")
-    //    .forEach { id ->
-    //        client.queryText("/api/sense/", id)?.let { println("[JSON SENSE] $it") }
-    //    }
-//
-    //listOf("row,n-1")
-    //    .forEach { id ->
-    //        client.queryText("/api/lex/", id)?.let { println("[JSON LEX] $it") }
-    //    }
-//
-    //listOf("row")
-    //    .forEach { id ->
-    //        client.queryText("/api/word/", id)?.let { println("[JSON LEMMA] $it") }
-    //    }
+    listOf("row%1:14:00::")
+        .forEach { id ->
+            allOptions.forEach { options ->
+                client.queryText("/api/sense/", id, options = options)?.let { println("[JSON SENSE $options]\n$it") }
+            }
+        }
+
+    listOf("row,n-1")
+        .forEach { id ->
+            allOptions.forEach { options ->
+                client.queryText("/api/lex/", id, options = options)?.let { println("[JSON LEX $options]\n$it") }
+            }
+        }
+
+    listOf("row")
+        .forEach { id ->
+            allOptions.forEach { options ->
+                client.queryText("/api/word/", id, options = options)?.let { println("[JSON LEMMA $options]\n$it") }
+            }
+        }
+}
+
+fun queryText() {
+    val client = Client(endpoint)
+    client.queryText("/", "")?.let { println("[OEWN] $it") }
+
+    listOf("00001740-a", "00001740-n", "00001740-r", "00001740-v")
+        .forEach { id ->
+            allOptions.forEach { options ->
+                client.queryText("/api/synset/", id, options = options)?.let { println("[JSON SYNSET $options]\n$it") }
+            }
+        }
+
+    listOf("row%1:14:00::", "row%1:17:00::", "row%1:06:00::", "row%1:14:01::", "row%1:07:00::", "row%1:04:00::", "row%1:10:00::", "row%2:38:00::")
+        .forEach { id ->
+            allOptions.forEach { options ->
+                client.queryText("/api/sense/", id, options = options)?.let { println("[JSON SENSE $options]\n$it") }
+            }
+        }
+
+    listOf("row,n-1", "row,n-2", "row,v")
+        .forEach { id ->
+            allOptions.forEach { options ->
+                client.queryText("/api/lex/", id, options = options)?.let { println("[JSON LEX $options]\n$it") }
+            }
+        }
+
+    listOf("row", "grow")
+        .forEach { id ->
+            allOptions.forEach { options ->
+                client.queryText("/api/word/", id, options = options)?.let { println("[JSON LEMMA $options]\n$it") }
+            }
+        }
 }
 
 fun main(args: Array<String>) {
